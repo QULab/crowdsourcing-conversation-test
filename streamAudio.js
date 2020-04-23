836e97a41fefc180b0e9cdbe024f2cb3d51c099f
Pavan Chitradurga Thammanna <pavan.chitradurgathammanna@campus.tu-berlin.de>
2020-04-23 09:24:32 +0200

    fix Dockerfile

--- streamAudio.js
+++ streamAudio.js
@@ -4,19 +4,14 @@ const http = require('http');
 let filePath = './public/bot-test/StarWars60.wav';
 let stat = fs.statSync(filePath);
 
-//const musicStream = fs.createReadStream(process.argv[2]);
-
 http
   .createServer((request, response) => {
-    //var queryData = url.parse(request.url, true).query;
-    // const skip = typeof(queryData.skip) == 'undefined' ? 0 : queryData.skip;
-    // const startByte = stat.size * skip;
 
     response.writeHead(200, {
         'Content-Type': 'audio/wav',
     });
 
-    // We replaced all the event handlers with a simple call to util.pump()
+
     fs.createReadStream(filePath).pipe(response);
   })
   .listen(8080);
