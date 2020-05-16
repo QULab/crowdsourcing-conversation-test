ae87e1cd3eff71edd274d289ceb79ff08d6deba4
Pavan Chitradurga Thammanna <pavan.chitradurgathammanna@campus.tu-berlin.de>
2020-05-16 04:56:32 +0200

    add copy to clipboard

--- public/user-test/main.js
+++ public/user-test/main.js
@@ -29,6 +29,8 @@ let packetsLost;
 let packetLossArray = [];
 let averagePacktLoss;
 
+/* globals MediaRecorder */
+
 let browser = (function (agent) {
   switch (true) {
     case agent.indexOf("edge") > -1: return "edge";
@@ -178,9 +180,56 @@ function sendData() {
     var hash = CryptoJS.MD5("Message").toString();
     //alert(hash);
     // $('#myModalTitle').html(myTitle);
-    $('#modalBodyMessage').html("Please copy verification code: " + "<div style='color: #0275d8;'> " + hash + "</div>");
+    $('#modalBodyMessage')
+    .html("Verification code: " + "<div id='verificationCode' style='color: #0275d8;'> " + hash + "</div>");
     //$('#modalBodyVerificationCode').html(hash);
     $('#exampleModal').modal('show');
+
+    // $(".modal").on("shown.bs.modal", function () {
+    //   console.log('a', Clipboard, $('#copy'), $("#copy-input").val());
+    //   var clipboard = new Clipboard('#copy')
+    // });
+
+    // Tooltip
+
+    $('#copy').tooltip({
+      trigger: 'click',
+      placement: 'bottom'
+    });
+
+    function setTooltip(btn, message) {
+      btn.tooltip('hide')
+        .attr('data-original-title', message)
+        .tooltip('show');
+    }
+
+    function hideTooltip(btn) {
+      setTimeout(function () {
+        btn.tooltip('hide');
+      }, 1000);
+    }
+
+// Clipboard
+
+    var clipboard = new ClipboardJS('#copy');
+
+    clipboard.on('success', function (e) {
+      console.info('Action:', e.action);
+      console.info('Text:', e.text);
+      console.info('Trigger:', e.trigger);
+      var btn = $(e.trigger);
+      setTooltip(btn, 'Copied!');
+
+      e.clearSelection();
+    });
+
+    clipboard.on('error', function (e) {
+      setTooltip('Failed!');
+      console.error('Action:', e.action);
+      console.error('Trigger:', e.trigger);
+      var btn = $(e.trigger);
+      hideTooltip(btn);
+    });
 
     // post data to backend after hangup
     const data = {
@@ -354,5 +403,6 @@ function gotLocalMediaStream(mediaStream) {
   localStreamNode.connect(outgoingRemoteGainNode);
   console.log('Connected localStreamNode.');
 }
+
 
 
--- public/user-user/js/app.js
+++ public/user-user/js/app.js
@@ -301,9 +301,52 @@ function hangup() {
 function sendData() {
     // show the thank you message
     var hash = CryptoJS.MD5("Message").toString();
-    $('#modalBodyMessage').html("Please copy verification code: " + "<div style='color: #0275d8;'> " + hash + "</div>");
+    $('#modalBodyMessage')
+        .html("Verification code: " + "<div id='verificationCode' style='color: #0275d8;'> " + hash + "</div>");
     //$('#modalBodyVerificationCode').html(hash);
     $('#exampleModal').modal('show');
+
+    // Tooltip
+
+    $('#copy').tooltip({
+        trigger: 'click',
+        placement: 'bottom'
+    });
+
+    function setTooltip(btn, message) {
+        btn.tooltip('hide')
+            .attr('data-original-title', message)
+            .tooltip('show');
+    }
+
+    function hideTooltip(btn) {
+        setTimeout(function () {
+            btn.tooltip('hide');
+        }, 1000);
+    }
+
+    // Clipboard
+
+    var clipboard = new ClipboardJS('#copy');
+
+    clipboard.on('success', function (e) {
+        console.info('Action:', e.action);
+        console.info('Text:', e.text);
+        console.info('Trigger:', e.trigger);
+        var btn = $(e.trigger);
+        setTooltip(btn, 'Copied!');
+
+        e.clearSelection();
+    });
+
+    clipboard.on('error', function (e) {
+        setTooltip('Failed!');
+        console.error('Action:', e.action);
+        console.error('Trigger:', e.trigger);
+        var btn = $(e.trigger);
+        hideTooltip(btn);
+    });
+
     if(rttArr.length){
         // post data to backend after hangup
         const data = {
